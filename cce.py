89e9075fcb5537e71d330797e289a0519fd724cc
Lucas C <lcosier@ethz.ch>
2023-04-06 14:44:16 +0200

    update metrics

--- cce.py
+++ cce.py
@@ -1,36 +1,19 @@
 import numpy as np
 
-def compute_jerk(position, time):
+def jerk(position, time):
+    """
+    Calculates the jerk of a trajectory. Returns the sum of the absolute values for each sphere on the EE.
+    """
     velocity = np.array([np.gradient(position[..., i], time) for i in range(3)]).transpose()
     acceleration = np.array([np.gradient(velocity[..., i], time) for i in range(3)]).transpose()
     jerk = np.array([np.gradient(acceleration[..., i], time) for i in range(3)]).transpose()
-    return np.sum(np.abs(jerk))
+    return np.sum(np.linalg.norm(jerk, axis=1))
 
-def compute_path_length(position):
-    distances = np.sqrt(np.sum(np.diff(position, axis=0)**2, axis=1))
-    path_length = np.sum(distances)
-    return path_length
-
-def compute_total_variation(position, time):
-    velocity = np.array([np.gradient(position[..., i], time) for i in range(3)]).transpose()
-    total_variation = np.sum(np.abs(np.array([np.gradient(velocity[..., i], time) for i in range(3)]).transpose()))
-    return total_variation
-
-def jerk_jk(position, time):
-    velocity = np.diff(position, axis=0) / np.diff(time)[..., np.newaxis]
-    acceleration = np.diff(velocity, axis=0) / np.diff(time[1:])[..., np.newaxis]
-    jerk = np.diff(acceleration, axis=0) / np.diff(time[2:])[..., np.newaxis]
-    return np.sum(np.abs(jerk))
-
-def path_length_jk(position):
+def path_length(position):
     distance = np.linalg.norm(np.diff(position, axis=0), axis=1)
     return np.sum(distance)
 
-def total_variation_jk(position, time):
-    velocity = np.diff(position, axis=0) / np.diff(time)[..., np.newaxis]
-    return np.sum(np.abs(np.diff(velocity, axis=0)))
-
-def total_variation_jj(traj, timesteps):
+def total_variation(traj, timesteps):
     """
     Calculates the total variation of a trajectory.
     :param traj: An array of values representing the trajectory. Shape: (N, 3)
@@ -41,6 +24,14 @@ def total_variation_jj(traj, timesteps):
     time_diffs = np.diff(timesteps)
     velocities = np.linalg.norm(diffs / time_diffs[:, np.newaxis], axis=1)
     return np.sum(velocities)
+
+def average_metrics(metrics_dict):
+    avg_dict = []
+    for key in metrics_dict.keys():
+        avg_dict.append(np.mean(metrics_dict[key]))
+
+    return np.mean(avg_dict), np.sqrt(np.std(avg_dict)) * 2
+
 degree_of_freedom = 3
 end_time = 1
 scale = 100
@@ -53,40 +44,33 @@ to_keep_jerk = defaultdict(list)
 to_keep_tv = defaultdict(list)
 robot = "franka"
 problemset = "bookshelves"
+print("Robot: {}, Problemset: {}".format(robot, problemset))
 max_jerk = float('-inf')
 for i in range(5):
     for j in range(55):
         # Load the data
         try:
-            position = np.load('/home/freshpate/saved_paths/{}/{}/pair_{}_run_{}.npy'.format(robot, problemset, j, i))
+            position = np.load('/home/lucasc/git/vgpmp/data/saved_paths/{}/{}/pair_{}_run_{}.npy'.format(robot, problemset, j, i))
         except:
             continue
         # time = np.load('data/time_{}_{}.npy'.format(i, j))
         # Compute the metrics
-        jerk = compute_jerk(position, time)
-        path_length = compute_path_length(position)
-        total_variation = compute_total_variation(position, time)
-        # Save the metrics
-        see = jerk_jk(position, time)
-        see2 = path_length_jk(position)
-        see3 = total_variation_jj(position, time)
-        see4 = total_variation_jk(position, time)
-        print(see3, see4)
+        jk = jerk(position, time)
+        pl = path_length(position)
+        tv = total_variation(position, time)
+
+        # print(see3, see4)
         # np.save('data/jerk_{}_{}.npy'.format(i, j), jerk)
         # np.save('data/path_length_{}_{}.npy'.format(i, j), path_length)
         # np.save('data/total_variation_{}_{}.npy'.format(i, j), total_variation)
         # keep metrics in the dictionary
-        max_jerk = max(max_jerk, np.max(np.abs(jerk)))
-        to_keep_length[j].append(path_length)
-        to_keep_jerk[j].append(jerk)
-        to_keep_tv[i].append(total_variation)
+        # max_jerk = max(max_jerk, np.max(np.abs(jerk)))
 
-# print(to_keep_tv)
-# print(max_jerk)
-avg_tv = defaultdict(float)
-std_tv = defaultdict(float)
-for key in to_keep_tv.keys():
-    avg_tv[key] = np.mean(to_keep_tv[key])
-    std_tv[key] = np.std(to_keep_tv[key])
-print(avg_tv)
-print(std_tv)
+        to_keep_length[j].append(pl)
+        to_keep_jerk[j].append(jk)
+        to_keep_tv[i].append(tv)
+
+
+print("Average jerk: {}, std {}".format(*average_metrics(to_keep_jerk)))
+print("Average path length: {}, std {}".format(*average_metrics(to_keep_length)))
+print("Average total variation: {}, std {}".format(*average_metrics(to_keep_tv)))
