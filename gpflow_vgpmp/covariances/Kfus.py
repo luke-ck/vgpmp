added dispatchers for correct conditioning and kernel derivatives / added new classes for ind vars and kernels

gpflow_vgpmp/covariances/Kfus.py
@@ -11,18 +11,19 @@ from gpflow.base import TensorLike
 from gpflow_sampling.covariances.Kfus import Kfu
 
 from gpflow_vgpmp.covariances.Kufs import Kuf as Kuf_dispatch
-from ..inducing_variables.inducing_variables import InducingVariables
 
+from gpflow.inducing_variables.multioutput.inducing_variables import SharedIndependentInducingVariables, SeparateIndependentInducingVariables
+from gpflow_vgpmp.covariances.Kufs import Kuf
+from gpflow_vgpmp.kernels.kernels import FirstOrderKernelDerivativeSeparateIndependent, VanillaConditioningSeparateIndependent
 
 # ==============================================
 #                     Kfus
 # ==============================================
 
 
-@Kfu.register(InducingVariables, kernels.MultioutputKernel, TensorLike)
+@Kfu.register(SharedIndependentInducingVariables, FirstOrderKernelDerivativeSeparateIndependent, TensorLike)
 def _Kfu_fallback_multioutput(Z, kern, X, **kwargs):
     Kuf = Kuf_dispatch(Z, kern, X, **kwargs)
-    print("dupa mizerie", Kuf.shape)
     # Assume features of x and z are 1-dimensional
     ndims_x = X.shape.ndims - 1  # assume x lives in 1d space
     ndims_z = 1  # shared Z live in 1d space, separate Z are 2d but 1-to-1 with L
@@ -31,5 +32,49 @@ def _Kfu_fallback_multioutput(Z, kern, X, **kwargs):
     # Swap the batch axes of x and z
     axes = list(range(1, ndims_x + ndims_z + 1))  # keep L output-features first
     perm = [0] + axes[ndims_z: ndims_z + ndims_x] + axes[:ndims_z]
-    new_Kuf = tf.concat([-Kuf[:, :2], Kuf[:, 2:]], axis=1)
+    len_ny = len(Z.inducing_variable.ny)
+    new_Kuf = tf.concat([-Kuf[:, :len_ny], Kuf[:, len_ny:]], axis=1)
     return tf.transpose(new_Kuf, perm)
+
+
+@Kfu.register(SharedIndependentInducingVariables, VanillaConditioningSeparateIndependent, TensorLike)
+def _Kfu_fallback_multioutput(Z, kern, X, **kwargs):
+    Kuf = Kuf_dispatch(Z, kern, X, **kwargs)
+    # Assume features of x and z are 1-dimensional
+    ndims_x = X.shape.ndims - 1  # assume x lives in 1d space
+    ndims_z = 1  # shared Z live in 1d space, separate Z are 2d but 1-to-1 with L
+    assert ndims_x + ndims_z == Kuf.shape.ndims - 1
+
+    # Swap the batch axes of x and z
+    axes = list(range(1, ndims_x + ndims_z + 1))  # keep L output-features first
+    perm = [0] + axes[ndims_z: ndims_z + ndims_x] + axes[:ndims_z]
+    return tf.transpose(Kuf, perm)
+
+@Kfu.register(SeparateIndependentInducingVariables, FirstOrderKernelDerivativeSeparateIndependent, TensorLike)
+def _Kfu_fallback_multioutput(Z, kern, X, **kwargs):
+    Kuf = Kuf_dispatch(Z, kern, X, **kwargs)
+    # Assume features of x and z are 1-dimensional
+    ndims_x = X.shape.ndims - 1  # assume x lives in 1d space
+    ndims_z = 1  # shared Z live in 1d space, separate Z are 2d but 1-to-1 with L
+    assert ndims_x + ndims_z == Kuf.shape.ndims - 1
+
+    # Swap the batch axes of x and z
+    axes = list(range(1, ndims_x + ndims_z + 1))  # keep L output-features first
+    perm = [0] + axes[ndims_z: ndims_z + ndims_x] + axes[:ndims_z]
+    len_ny = len(Z.inducing_variable.ny)
+    new_Kuf = tf.concat([-Kuf[:, :len_ny], Kuf[:, len_ny:]], axis=1)
+    return tf.transpose(new_Kuf, perm)
+
+
+@Kfu.register(SeparateIndependentInducingVariables, VanillaConditioningSeparateIndependent, TensorLike)
+def _Kfu_fallback_multioutput(Z, kern, X, **kwargs):
+    Kuf = Kuf_dispatch(Z, kern, X, **kwargs)
+    # Assume features of x and z are 1-dimensional
+    ndims_x = X.shape.ndims - 1  # assume x lives in 1d space
+    ndims_z = 1  # shared Z live in 1d space, separate Z are 2d but 1-to-1 with L
+    assert ndims_x + ndims_z == Kuf.shape.ndims - 1
+
+    # Swap the batch axes of x and z
+    axes = list(range(1, ndims_x + ndims_z + 1))  # keep L output-features first
+    perm = [0] + axes[ndims_z: ndims_z + ndims_x] + axes[:ndims_z]
+    return tf.transpose(Kuf, perm)

gpflow_vgpmp/covariances/Kuus.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 from gpflow.covariances.dispatch import Kuu
-from gpflow.kernels import SquaredExponential, Kernel
+from gpflow.kernels import Kernel
 
-from ..inducing_variables.inducing_variables import VariableInducingPoints, InducingPointsInterface
+from ..inducing_variables.inducing_variables import InducingPointsInterface
 
 
 @Kuu.register(InducingPointsInterface, Kernel)
-def Kuu_kernel_inducingpoints(inducing_variable: VariableInducingPoints, kernel: SquaredExponential, *, jitter=0.0):
-    padding = 2
+def Kuu_kernel_inducingpoints(inducing_variable: InducingPointsInterface, kernel: Kernel, *, jitter=0.0):
+    padding = inducing_variable.ny.shape[0]
     Kzz = kernel(inducing_variable.Zy)  # this computes k([u, y].T)
     Kzz += jitter * tf.eye(inducing_variable.num_inducing + padding, dtype=Kzz.dtype)
     return Kzz

gpflow_vgpmp/covariances/multioutput/Kufs.py
@@ -1,27 +1,56 @@
 import tensorflow as tf
-from gpflow.inducing_variables.multioutput.inducing_variables import SharedIndependentInducingVariables
+from gpflow.inducing_variables.multioutput.inducing_variables import SharedIndependentInducingVariables, SeparateIndependentInducingVariables
 from gpflow.kernels.multioutput.kernels import SeparateIndependent, SharedIndependent
-
+from gpflow_vgpmp.inducing_variables.inducing_variables import ConditionedVelocitySharedIndependentInducingVariables
 from gpflow_vgpmp.covariances.Kufs import Kuf
+from gpflow_vgpmp.kernel_conditioning.dispatch import K_conditioned
 from gpflow_vgpmp.derivatives.dispatch import K_grad
+from gpflow_vgpmp.kernels.kernels import FirstOrderKernelDerivativeSeparateIndependent, VanillaConditioningSeparateIndependent, VanillaConditioningSharedIndependent
+from gpflow.base import TensorLike
 
-
-@Kuf.register(SharedIndependentInducingVariables, SeparateIndependent, object)
+@Kuf.register(SharedIndependentInducingVariables, FirstOrderKernelDerivativeSeparateIndependent, TensorLike)
 def Kuf_fallback_shared_separate(
         inducing_variable: SharedIndependentInducingVariables,
-        kernel: SeparateIndependent,
+        kernel: FirstOrderKernelDerivativeSeparateIndependent,
         Xnew: tf.Tensor,
 ):
-    kuf = tf.stack(
-        [Kuf(inducing_variable.inducing_variable, k, Xnew) for k in kernel.kernels], axis=0
-    )  # [L, M+2, P]
+	kuf = K_conditioned(inducing_variable.inducing_variable.Zy,
+                         Xnew, kernel) # [L, M+2, P]
+	ny_x_block = K_grad(inducing_variable.inducing_variable.ny, Xnew, kernel)  # [L, 2, P]
+	return tf.concat([ny_x_block, kuf], axis=1)  # [L, M+4, P]
+
+@Kuf.register(SharedIndependentInducingVariables, VanillaConditioningSeparateIndependent, TensorLike)
+def Kuf_fallback_shared_separate(
+        inducing_variable: SharedIndependentInducingVariables,
+        kernel: VanillaConditioningSeparateIndependent,
+        Xnew: tf.Tensor,
+):
+    kuf = K_conditioned(inducing_variable.inducing_variable.Zy,
+                         Xnew, kernel)   # [L, 2, P]
+    return kuf
+
+@Kuf.register(SeparateIndependentInducingVariables, FirstOrderKernelDerivativeSeparateIndependent, TensorLike)
+def Kuf_fallback_shared_separate(
+        inducing_variable: SharedIndependentInducingVariables,
+        kernel: FirstOrderKernelDerivativeSeparateIndependent,
+        Xnew: tf.Tensor,
+):
+    kuf = K_conditioned(inducing_variable.inducing_variable.Zy,
+                         Xnew, kernel)   # [L, M+2, P]
     ny_x_block = K_grad(inducing_variable.inducing_variable.ny, Xnew, kernel)  # [L, 2, P]
-    # kuf = tf.pad(kuf, [[0, 0], [2, 0], [0, 0]])
     return tf.concat([ny_x_block, kuf], axis=1)  # [L, M+4, P]
-    # return kuf
 
+@Kuf.register(SeparateIndependentInducingVariables, VanillaConditioningSeparateIndependent, TensorLike)
+def Kuf_fallback_shared_separate(
+        inducing_variable: SharedIndependentInducingVariables,
+        kernel: VanillaConditioningSeparateIndependent,
+        Xnew: tf.Tensor,
+):
+    kuf = K_conditioned(inducing_variable.inducing_variable.Zy,
+                         Xnew, kernel)   # [L, 2, P]
+    return kuf
 
-@Kuf.register(SharedIndependentInducingVariables, SharedIndependent, object)
+@Kuf.register(SharedIndependentInducingVariables, SharedIndependent, TensorLike)
 def Kuf_fallback_shared_shared(
         inducing_variable: SharedIndependentInducingVariables,
         kernel: SharedIndependent,

gpflow_vgpmp/covariances/multioutput/Kuus.py
@@ -2,23 +2,23 @@ from typing import Union
 
 import tensorflow as tf
 from gpflow.covariances.dispatch import Kuu
-from gpflow.inducing_variables.multioutput.inducing_variables import FallbackSharedIndependentInducingVariables
+from gpflow.inducing_variables.multioutput.inducing_variables import FallbackSharedIndependentInducingVariables, FallbackSeparateIndependentInducingVariables
 from gpflow.kernels.multioutput.kernels import (SeparateIndependent, IndependentLatent, SharedIndependent)
-
+from gpflow_vgpmp.inducing_variables.inducing_variables import (ConditionedSeparateIndependentInducingVariables, ConditionedSharedIndependentInducingVariables, ConditionedVelocitySharedIndependentInducingVariables)
+from gpflow_vgpmp.kernel_conditioning.dispatch import K_conditioned
 from gpflow_vgpmp.derivatives.dispatch import K_grad, K_grad_grad
+from gpflow_vgpmp.kernels.kernels import FirstOrderKernelDerivativeSeparateIndependent, VanillaConditioningSeparateIndependent
 
 
-@Kuu.register(FallbackSharedIndependentInducingVariables, (SeparateIndependent, IndependentLatent))
+@Kuu.register(FallbackSharedIndependentInducingVariables, (FirstOrderKernelDerivativeSeparateIndependent, IndependentLatent))
 def Kuu_fallback_shared_separate(
-        inducing_variable: FallbackSharedIndependentInducingVariables,
-        kernel: Union[SeparateIndependent, IndependentLatent],
+        inducing_variable: ConditionedSharedIndependentInducingVariables,
+        kernel: Union[FirstOrderKernelDerivativeSeparateIndependent, IndependentLatent],
         *,
         jitter: float = 0.0,
 ) -> tf.Tensor:
-    Kmm = tf.stack(
-        [Kuu(inducing_variable.inducing_variable, k) for k in kernel.kernels], axis=0
-    )  # [L, M+2, M+2]
-    # Kzz = tf.pad(Kmm, [[0, 0], [2, 0], [2, 0]])
+    Kmm = K_conditioned(inducing_variable.inducing_variable.Zy,
+                         inducing_variable.inducing_variable.Zy, kernel)
     zy_ny_block = K_grad(inducing_variable.inducing_variable.Zy,
                          inducing_variable.inducing_variable.ny, kernel)  # [L, M+2, 2]
     ny_zy_block = K_grad(inducing_variable.inducing_variable.ny,
@@ -29,18 +29,68 @@ def Kuu_fallback_shared_separate(
     up_block = tf.concat([ny_ny_block, ny_zy_block], axis=-1)  # [L, 2, M+4]
     Kmm = tf.concat([up_block, Kmm], axis=1)  # [L, M+4, M+4]
 
-    jittermat = tf.eye(inducing_variable.num_inducing + 4 , dtype=Kmm.dtype)[None, :, :] * jitter
+    padding = inducing_variable.inducing_variable.ny.shape[0] + inducing_variable.inducing_variable.dny.shape[0]
+    jittermat = tf.eye(inducing_variable.num_inducing + padding , dtype=Kmm.dtype)[None, :, :] * jitter
     return Kmm + jittermat
 
+@Kuu.register(FallbackSharedIndependentInducingVariables, (VanillaConditioningSeparateIndependent, IndependentLatent))
+def Kuu_fallback_shared_separate(
+        inducing_variable: FallbackSharedIndependentInducingVariables,
+        kernel: Union[VanillaConditioningSeparateIndependent, IndependentLatent],
+        *,
+        jitter: float = 0.0,
+) -> tf.Tensor:
+
+    Kmm = K_conditioned(inducing_variable.inducing_variable.Zy,
+                         inducing_variable.inducing_variable.Zy, kernel)
+    jittermat = tf.eye(inducing_variable.num_inducing + inducing_variable.inducing_variable.ny.shape[0], dtype=Kmm.dtype)[None, :, :] * jitter
+    return Kmm + jittermat
+
+@Kuu.register(FallbackSeparateIndependentInducingVariables, (FirstOrderKernelDerivativeSeparateIndependent, IndependentLatent))
+def Kuu_fallback_shared_separate(
+        inducing_variable: ConditionedSharedIndependentInducingVariables,
+        kernel: Union[FirstOrderKernelDerivativeSeparateIndependent, IndependentLatent],
+        *,
+        jitter: float = 0.0,
+) -> tf.Tensor:
+    Kmm = K_conditioned(inducing_variable.inducing_variable.Zy,
+                         inducing_variable.inducing_variable.Zy, kernel)
+    zy_ny_block = K_grad(inducing_variable.inducing_variable.Zy,
+                         inducing_variable.inducing_variable.ny, kernel)  # [L, M+2, 2]
+    ny_zy_block = K_grad(inducing_variable.inducing_variable.ny,
+                         inducing_variable.inducing_variable.Zy, kernel)  # [L, 2, M+2]
+
+    ny_ny_block = K_grad_grad(inducing_variable.inducing_variable.ny, kernel)  # [L, 2, 2]
+    Kmm = tf.concat([zy_ny_block, Kmm], axis=-1)  # [L, M+2, M+4] (down block)
+    up_block = tf.concat([ny_ny_block, ny_zy_block], axis=-1)  # [L, 2, M+4]
+    Kmm = tf.concat([up_block, Kmm], axis=1)  # [L, M+4, M+4]
+
+    padding = inducing_variable.inducing_variable.ny.shape[0] + inducing_variable.inducing_variable.dny.shape[0]
+    jittermat = tf.eye(inducing_variable.num_inducing + padding , dtype=Kmm.dtype)[None, :, :] * jitter
+    return Kmm + jittermat
+
+@Kuu.register(FallbackSeparateIndependentInducingVariables, (VanillaConditioningSeparateIndependent, IndependentLatent))
+def Kuu_fallback_shared_separate(
+        inducing_variable: FallbackSharedIndependentInducingVariables,
+        kernel: Union[VanillaConditioningSeparateIndependent, IndependentLatent],
+        *,
+        jitter: float = 0.0,
+) -> tf.Tensor:
+
+    Kmm = K_conditioned(inducing_variable.inducing_variable.Zy,
+                         inducing_variable.inducing_variable.Zy, kernel)
+    jittermat = tf.eye(inducing_variable.num_inducing + inducing_variable.inducing_variable.ny.shape[0], dtype=Kmm.dtype)[None, :, :] * jitter
+    return Kmm + jittermat
 
 @Kuu.register(FallbackSharedIndependentInducingVariables, SharedIndependent)
 def Kuu_fallback_shared_shared(
         inducing_variable: FallbackSharedIndependentInducingVariables,
-        kernel: Union[SeparateIndependent, IndependentLatent],
+        kernel: Union[SharedIndependent, IndependentLatent],
         *,
         jitter: float = 0.0,
 ) -> tf.Tensor:
     Kmm = Kuu(inducing_variable.inducing_variable, kernel.kernel)
 
-    jittermat = tf.eye(inducing_variable.num_inducing + 2, dtype=Kmm.dtype) * jitter
+    jittermat = tf.eye(inducing_variable.num_inducing + inducing_variable.inducing_variable.ny.shape[0], dtype=Kmm.dtype) * jitter
     return Kmm + jittermat
+